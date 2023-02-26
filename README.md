91f88173c869ec12474f5965881e7e10bd979684
Quinn Hertel <98055166+egg-bagel@users.noreply.github.com>
2023-02-26 09:16:47 -0500

    Game is working, including save and load options

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # hangman
 Hangman game from TOP Ruby course
+
+This Hangman game is played in the terminal. The program loads a list of 10,000 words from an external file and randomly chooses one of them between 5-12 words for the player to guess. The player has 6 wrong guesses before they lose the game. Every turn, an updated array prints to the screen displaying which letters the player has guessed correctly so far. Once the player has guessed a letter, it is removed from the list of valid choices, so it cannot be guessed twice.
+
+Here is the word list I used for this game: https://raw.githubusercontent.com/first20hours/google-10000-english/master/google-10000-english-no-swears.txt. I downloaded the file and added it to my project folder to have my program read from it.
+
+The game can be saved and returned to at any point. If the player types "save" instead of entering a letter, the program prompts the player to provide a name for their save file. The current game state is then serialized using YAML. When the player returns to re-open the YAML file later, it picks up at exactly the same point where the game left off. 
+
+This was fun to make and fun to play. :)
--- script.rb
+++ script.rb
@@ -1,3 +1,5 @@
+require "yaml"
+
 class Game
   
   def initialize
@@ -6,8 +8,14 @@ class Game
     @letters_available = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j",
                           "k", "l", "m", "n", "o", "p", "q", "r", "s", "t",
                           "u", "v", "w", "x", "y", "z"]
+    @guess = []
     @wrong_guesses_left = 6
     puts "Hi! Welcome to my Hangman game."
+    print_hangman
+    puts "\n"
+    puts "Your word to guess is #{@secret_word.length} letters long."
+    puts "If you would like to save the game at any point,"
+    puts "type \"save\" when the program prompts you to enter a letter."
   end
   
   #Randomly chooses a word 5-12 letters long from the dictionary.
@@ -20,16 +28,17 @@ class Game
   end
 
   def play
-    print_hangman
-    puts "\n"
-    puts "Your word to guess is #{@secret_word.length} letters long."
-    make_guess_array
 
     while @wrong_guesses_left > 0
+      print_guess_array
       letter = guess_letter
+      until @letters_available.include?(letter)
+        letter = guess_letter
+      end
       is_letter_in_word(letter)
       if @guess.join("").gsub(" ", "") == @secret_word.join("")
         puts "You win! You guessed the word correctly."
+        puts @guess.join("")
         break
       end
     end
@@ -42,13 +51,14 @@ class Game
 
   end
 
-  #Makes a new empty array to be filled in as the player guesses letters.
-  def make_guess_array
-    @guess = Array.new(@secret_word.length)
-    i = 0
-    while i < @secret_word.length
-      @guess[i] = "_ "
-      i += 1
+  #Prints out the array that represents the player's guess
+  def print_guess_array
+    if @guess.empty?
+      i = 0
+      while i < @secret_word.length
+        @guess[i] = "_ "
+        i += 1
+      end
     end
     puts @guess.join
   end
@@ -59,9 +69,14 @@ class Game
     puts "Available letters: #{@letters_available.join(" ")}"
     puts "Guess a letter: "
     letter = gets.chomp.downcase
-    until @letters_available.include?(letter)
-      puts "Invalid input, please try again: "
-      letter = gets.chomp.downcase
+    if letter == "save"
+      save_game
+      return
+    else
+      until @letters_available.include?(letter)
+        puts "Invalid input, please try again: "
+        letter = gets.chomp.downcase
+      end
     end
     return letter
   end
@@ -71,7 +86,7 @@ class Game
     if @secret_word.include?(letter)
       fill_in_blanks(letter)
     else
-      puts "\"#{letter}\" is not included in the secret word."
+      puts "\"#{letter}\" is not in the secret word."
       @wrong_guesses_left -= 1
       print_hangman
     end
@@ -85,7 +100,6 @@ class Game
         @guess[ind] = "#{letter} "
       end
     end
-    puts @guess.join("")
   end
 
   #Prints the hangman to the screen.
@@ -105,7 +119,6 @@ class Game
       puts " |     "
       puts "_|_    "
       puts "\n"
-      puts @guess.join("")
     elsif @wrong_guesses_left == 4
       puts " ______"
       puts " |    |"
@@ -114,7 +127,6 @@ class Game
       puts " |     "
       puts "_|_    "
       puts "\n"
-      puts @guess.join("")
     elsif @wrong_guesses_left == 3
       puts " ______"
       puts " |    |"
@@ -123,7 +135,6 @@ class Game
       puts " |     "
       puts "_|_    "
       puts "\n"
-      puts @guess.join("")
     elsif @wrong_guesses_left == 2
       puts " ______"
       puts " |    |"
@@ -132,7 +143,6 @@ class Game
       puts " |     "
       puts "_|_    "
       puts "\n"
-      puts @guess.join("")
     elsif @wrong_guesses_left == 1
       puts " ______"
       puts " |    |"
@@ -141,7 +151,6 @@ class Game
       puts " |   / "
       puts "_|_    "
       puts "\n"
-      puts @guess.join("")
     else
       puts " ______"
       puts " |    |"
@@ -149,9 +158,47 @@ class Game
       puts " |   /|\\"
       puts " |   / \\"
       puts "_|_    "
+      puts @guess.join("")
     end
   end
+
+  #Writes the current game state to a yaml file named by the player
+  def save_game
+    puts "What do you want to name your save file?"
+    file_name = gets.chomp
+    saved_game = File.open("#{file_name}.yml", "w") { |file| file.write(self.to_yaml)}
+  end
+
 end
 
-new_game = Game.new
-new_game.play
+play_again = "Y"
+while play_again == "Y"
+
+  puts "Would you like to load a previously saved game?"
+  puts "Enter Y for yes. Enter N to start a new game."
+  load_choice = gets.chomp.upcase
+  until load_choice == "Y" || load_choice == "N"
+    puts "Invalid input, please try again:"
+    load_choice = gets.chomp.upcase
+  end
+
+  if load_choice == "Y"
+    begin
+      puts "Enter the name of the saved game file you want to open."
+      saved_file_name = gets.chomp    
+      new_game = YAML::load(File.read("#{saved_file_name}.yml"), permitted_classes: [Game] )
+      new_game.print_hangman
+      new_game.play
+    rescue
+      puts "Sorry, I couldn't find that file."
+    end
+  end
+
+  if load_choice == "N"
+    new_game = Game.new
+    new_game.play
+  end
+
+puts "Would you like to play again? Y/N"
+play_again = gets.chomp.upcase
+end
